e51a20878404b1a1db1b8af2a4e25ffa5903b32f
Stephen Galvin <stephenegalvin@gmail.com>
2021-02-22 14:11:59 -0500

    first commit

---
+++ scripts/document_elements.js
@@ -0,0 +1,4 @@
+let root = document.documentElement;
+let pixel_guide = document.getElementById("pixel-guide");
+let canvas_holder = document.getElementById("canvas-holder");
+let pixel_canvas = document.getElementById("pixel-canvas");
---
+++ scripts/pixle_guide_scripts.js
@@ -0,0 +1,50 @@
+//Local Variables 
+var mouseX;
+var mouseY;
+var pixelSizeSkew = 24;
+var pixelArtHeight;
+var pixelArtWidth;
+var pixelGuideLeftPosition;
+var pixelGuideTopPosition;
+
+
+/*
+    Shows/Hides the Pixel Guide when the Mouse enters or leaves the Art Canvas 
+*/
+canvas_holder.addEventListener("mouseenter", e => {
+    pixel_guide.style.display = "block";
+});
+
+canvas_holder.addEventListener("mouseleave", e => {
+    pixel_guide.style.display = "none";
+});
+
+canvas_holder.addEventListener("mousemove", e => {
+    mouseX = e.clientX - 10;
+    mouseY = e.clientY - 10;
+
+    holdX = mouseX / pixelSizeSkew;
+    holdY = mouseY / pixelSizeSkew;
+
+    pixelGuideLeftPosition = Math.floor(holdX) * pixelSizeSkew;
+    pixelGuideTopPosition = Math.floor(holdY) * pixelSizeSkew;
+
+    pixel_guide.style.left = pixelGuideLeftPosition + "px";
+    pixel_guide.style.top = pixelGuideTopPosition + "px";
+});
+
+canvas_holder.addEventListener("mousedown", e => {
+    printVars();
+})
+
+
+/*
+    Functions 
+*/
+
+function printVars() {
+    console.log("mouseX: " + mouseX);
+    console.log("mouseY: " + mouseY);
+    console.log("pixelGuideLeftPosition: " + pixelGuideLeftPosition);
+    console.log("pixelGuideTopPosition: " + pixelGuideTopPosition);
+}
